a0c81c8672083acb8649dac624a3b70cadaa3949
Bek8866 <begi20010202@gmail.com>
2023-03-27 15:27:42 +0500

    third

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import MainPage from "./components/MainPage/MainPage";
 import WayOfLife from "./components/WayOfLife/Life";
 import Subs from "./components/Subsc/Subs";
 import Subsc2 from "./components/Subsc2/Subsc2";
-import Footer from "./components/Footer/Footer";
 import Reg from "./components/Register/Reg";
 import Map from "./components/Map/Map";
 import Footer2 from "./components/Footer2/Footer2";
-import BurgerFooter from "./components/footerBurger/FootBurger";
 function App() {
   return (
     <div>
@@ -17,8 +15,7 @@ function App() {
       <Subsc2 />
       <Reg />
       <Map />
-      {/* <BurgerFooter /> */}
-      <Footer2/>
+      <Footer2 />
     </div>
   );
 }
